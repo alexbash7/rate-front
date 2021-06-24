6f2cce345db508c854d267f2c0d97f1375cd2653
Oleksandr Bashynskyi <alexbash@Oleksandrs-MacBook-Air.local>
2021-06-24 17:11:24 +0300

    test

--- server.js
+++ server.js
@@ -8,5 +8,9 @@ const app = express();
 // app.use('/', serveStatic(path.join(__dirname, '/dist')));
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/people', function (req, res) {
+    res.send('hello');
+})
+
 const port = process.env.PORT || 8080;
 app.listen(port);
