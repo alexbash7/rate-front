3b6124289c47bbb653dcbe131bea2b55efb31f73
Oleksandr Bashynskyi <alexbash@Oleksandrs-MacBook-Air.local>
2021-06-24 17:07:06 +0300

    test

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const path = require('path');
 
 const app = express();
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
+// app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.use(express.static(__dirname + '/dist'));
 
 const port = process.env.PORT || 8080;
 app.listen(port);
